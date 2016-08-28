17767851324cebd4b4cca759f07cbbbb15a5d3b9
Matthew Barnard <msbarnard10@gmail.com>
2016-08-28 21:50:45 +0200

    fixed loads

--- app/assets/javascripts/administrator/projects.js
+++ app/assets/javascripts/administrator/projects.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function () {
-    alert('s')
+$(document).on('turbolinks:load', function () {
+    //alert('s')
     $('#projects_table').DataTable();
     $('[data-toggle="tooltip"]').tooltip();
 
